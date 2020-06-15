0b010e34b91801944b545885c9d76c0341fdb67d
liggest <liggest@sina.cn>
2020-06-15 23:22:35 +0800

    教程关 半成品
    基本实现了教程关卡（包括音效和BGM）
    但是背包部分和物品使用部分还没法做
    虽然检测了死亡点，但是死亡逻辑却没有写，所以没法复活

---
+++ Assets/Scripts/RebrithPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebrithPoint : MonoBehaviour
+{
+    public bool isWork = false;
+    //Vector3 point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //point = transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8) // 如果是Player
+        {
+            if (!isWork)
+            {
+                //Debug.Log($"激活{point}");
+                isWork = true;
+                GameCtrl.gc.AddWorkPoint(transform.position);
+            }
+        }
+    }
+}
