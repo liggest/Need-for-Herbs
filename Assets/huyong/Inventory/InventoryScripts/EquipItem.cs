ac1c6677562049fb7592a2383825934a47117a9b
liggest <liggest@sina.cn>
2020-06-16 00:15:34 +0800

    Revert "Revert "Inventory2.0""
    This reverts commit 843e07b5f39ae1d7dfe4aef38239ba2e452304f8.

---
+++ Assets/huyong/Inventory/InventoryScripts/EquipItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New PropItem",menuName ="Inventory/New EquipItem")]
+public class EquipItem : ScriptableObject {
+    public string itemName;
+    public Sprite itemImage;
+    public int Itemid;
+
+    [TextArea]
+    public string itemInfo;
+
+    public bool equip;
+}
